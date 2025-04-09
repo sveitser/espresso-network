2a904fa17838961cef130d0e87d7b371acaaea42
Mathis <sveitser@gmail.com>
2025-04-09 12:15:21 +0200

    fix: G2PointSol to BLSPubKey conversion (#2982)
    * fix: G2PointSol to BLSPubKey conversion
    
    * fix: use serialize uncompressed

--- contracts/rust/adapter/src/stake_table.rs
+++ contracts/rust/adapter/src/stake_table.rs
@@ -21,7 +21,7 @@ impl From<G2PointSol> for BLSPubKey {
             .into_group()
             .serialize_uncompressed(&mut bytes)
             .unwrap();
-        Self::deserialize_compressed(&bytes[..]).unwrap()
+        Self::deserialize_uncompressed(&bytes[..]).unwrap()
     }
 }
 
@@ -98,5 +98,35 @@ impl NodeInfoSol {
             schnorrVK: ark_ed_on_bn254::EdwardsAffine::rand(rng).into(),
             isDA: rng.gen_bool(0.2),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use hotshot_types::signature_key::{BLSPrivKey, BLSPubKey};
+
+    use crate::sol_types::G2PointSol;
+
+    fn check_round_trip(pk: BLSPubKey) {
+        let g2: G2PointSol = pk.to_affine().into();
+        let pk2: BLSPubKey = g2.into();
+        assert_eq!(pk2, pk, "Failed to roundtrip G2PointSol to BLSPubKey: {pk}");
+    }
+
+    #[test]
+    fn test_bls_g2_point_roundtrip() {
+        let mut rng = rand::thread_rng();
+        for _ in 0..100 {
+            let pk = (&BLSPrivKey::generate(&mut rng)).into();
+            check_round_trip(pk);
+        }
+    }
+
+    #[test]
+    fn test_bls_g2_point_alloy_migration_regression() {
+        // This pubkey fails the roundtrip if "serialize_{un,}compressed" are mixed
+        let s = "BLS_VER_KEY~JlRLUrn0T_MltAJXaaojwk_CnCgd0tyPny_IGdseMBLBPv9nWabIPAaS-aHmn0ARu5YZHJ7mfmGQ-alW42tkJM663Lse-Is80fyA1jnRxPsHcJDnO05oW1M1SC5LeE8sXITbuhmtG2JdTAgmLqWOxbMRmVIqS1AQXqvGGXdo5qpd";
+        let pk: BLSPubKey = s.parse().unwrap();
+        check_round_trip(pk);
     }
 }
