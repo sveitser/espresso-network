a470ef2cbe7e8df9938e81249ed097a7c482c60f
sveitser <sveitser@users.noreply.github.com>
2025-05-05 00:28:28 +0000

    deploy: aa2b5c764d561f7d98ae55f543ada290b9f63a7b

--- trait.impl/request_response/recipient_source/trait.RecipientSource.js
+++ trait.impl/request_response/recipient_source/trait.RecipientSource.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["sequencer",[["impl&lt;I: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeImplementation.html\" title=\"trait hotshot_types::traits::node_implementation::NodeImplementation\">NodeImplementation</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt;, V: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.Versions.html\" title=\"trait hotshot_types::traits::node_implementation::Versions\">Versions</a>&gt; <a class=\"trait\" href=\"request_response/recipient_source/trait.RecipientSource.html\" title=\"trait request_response::recipient_source::RecipientSource\">RecipientSource</a>&lt;<a class=\"enum\" href=\"sequencer/request_response/request/enum.Request.html\" title=\"enum sequencer::request_response::request::Request\">Request</a>, VerKey&gt; for <a class=\"struct\" href=\"sequencer/request_response/recipient_source/struct.RecipientSource.html\" title=\"struct sequencer::request_response::recipient_source::RecipientSource\">RecipientSource</a>&lt;I, V&gt;"]]]]);
+    var implementors = Object.fromEntries([["sequencer",[["impl&lt;I: NodeImplementation&lt;SeqTypes&gt;, V: Versions&gt; RecipientSource&lt;<a class=\"enum\" href=\"sequencer/request_response/request/enum.Request.html\" title=\"enum sequencer::request_response::request::Request\">Request</a>, VerKey&gt; for <a class=\"struct\" href=\"sequencer/request_response/recipient_source/struct.RecipientSource.html\" title=\"struct sequencer::request_response::recipient_source::RecipientSource\">RecipientSource</a>&lt;I, V&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[1106]}
+//{"start":57,"fragment_lengths":[483]}
