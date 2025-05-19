9d9c7426fdffaed3a3ffc37a04553475c631df72
sveitser <sveitser@users.noreply.github.com>
2025-05-19 00:28:37 +0000

    deploy: 9f6866873b7d2653fdfe496a0fa84852a06396e1

--- trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot_types",[["impl&lt;TYPES: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"hotshot_types/stake_table/struct.HSStakeTable.html\" title=\"struct hotshot_types::stake_table::HSStakeTable\">HSStakeTable</a>&lt;TYPES&gt;"]]]]);
+    var implementors = Object.fromEntries([["hotshot_types",[["impl&lt;TYPES: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"hotshot_types/stake_table/struct.HSStakeTable.html\" title=\"struct hotshot_types::stake_table::HSStakeTable\">HSStakeTable</a>&lt;TYPES&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
